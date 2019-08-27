9b431c88a6399bfe230f43b75a5cb6474b9f623b
Marcin Moskala <marcinmoskala@gmail.com>
2019-08-27 17:20:11 +0200

    Add files for Fib TDD excercise

--- src/main/kotlin/Test.kt
+++ src/main/kotlin/Test.kt
@@ -1,8 +1,3 @@
-import kotlinx.coroutines.coroutineScope
-import kotlinx.coroutines.delay
-import kotlinx.coroutines.launch
-import kotlin.concurrent.thread
-
 fun main() {
     print("Hello, world")
 }
---
+++ src/test/kotlin/_1_safety/FibTests.kt
@@ -0,0 +1,2 @@
+package _1_safety
+
