3c0903aa1d08f82e31738440a3669fdd2cf169dd
Marcin Moskala <marcinmoskala@gmail.com>
2019-08-27 12:54:31 +0200

    Improve requirements checks

--- src/main/kotlin/_1_safety/Requirements.kt
+++ src/main/kotlin/_1_safety/Requirements.kt
@@ -9,8 +9,21 @@ fun Notifier.notifyUser(user: User?) {
 class IncorrectId: Error()
 
 interface Notifier {
+
+    /**
+     * Indicate instance readiness to notify users
+     */
     val initialized: Boolean
-    fun initialize()
-    fun notifyPerson(id: Int)
+
+    /**
+     * Notifies person
+     * @param id Is an id of user we want to notify
+     * @return Was operation successful
+     */
+    fun notifyPerson(id: Int): Boolean
+
+    /**
+     * Checks if we can send message to the following `id`
+     */
     fun checkId(id: Int): Boolean
 }
--- src/test/kotlin/_1_safety/RequirementsChecks.kt
+++ src/test/kotlin/_1_safety/RequirementsChecks.kt
@@ -3,6 +3,7 @@ package _1_safety
 import _1_safety.RequirementsChecks.FakeNotifier.Companion.INCORRECT_ID
 import org.junit.Test
 import org.junit.jupiter.api.assertThrows
+import java.lang.AssertionError
 import kotlin.test.assertEquals
 import kotlin.test.assertNotNull
 import kotlin.test.assertTrue
@@ -34,7 +35,7 @@ class RequirementsChecks {
     fun `Function throws IllegalArgumentException when surname is null`() {
         val notifier = FakeNotifier(initialized = true)
         val id = 123
-        assertThrows<IllegalArgumentException> { notifier.notifyUser(User(id, "Mike", null)) }
+        assertThrows<IllegalArgumentException> { notifier.notifyUser(User(id, null, "Kowalski")) }
     }
 
     @Test
@@ -50,19 +51,32 @@ class RequirementsChecks {
         assertThrows<IncorrectId> { notifier.notifyUser(User(INCORRECT_ID, "Mike", "Bull")) }
     }
 
+    @Test
+    fun `It is expected that notifyPerson will return true to indicate success`() {
+        val notifier = FakeNotifier(initialized = true)
+        assertThrows<IncorrectId> { notifier.notifyUser(User(INCORRECT_ID, "Mike", "Bull")) }
+    }
+
+    @Test
+    fun `Function should check if notifyPerson returns true as expected in correct conditions`() {
+        val notifier = FakeNotifier(initialized = true)
+        val id = 123
+        notifier.incorrectImplementationMakingNotifyPersonReturnFalse = true
+        assertThrows<AssertionError> { notifier.notifyUser(User(id, "Ben", "Barack")) }
+    }
+
     class FakeNotifier(initialized: Boolean = false): Notifier {
         override var initialized = initialized
             private set
         var usersNotified = setOf<Int>()
-
-        override fun initialize() {
-            initialized = true
-        }
+        var incorrectImplementationMakingNotifyPersonReturnFalse = false
 
         override fun checkId(id: Int): Boolean = id != INCORRECT_ID
 
-        override fun notifyPerson(id: Int) {
+        override fun notifyPerson(id: Int): Boolean {
+            if(incorrectImplementationMakingNotifyPersonReturnFalse) return false
             usersNotified = usersNotified + id
+            return true
         }
         
         companion object {
