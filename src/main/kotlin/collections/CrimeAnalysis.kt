2fa338aa2a1491603ea3823b14ee789f5b96b432
marcinmoskala <marcinmoskala@gmail.com>
2021-10-27 14:58:07 +0200

    Correct link

--- src/main/kotlin/collections/CrimeAnalysis.kt
+++ src/main/kotlin/collections/CrimeAnalysis.kt
@@ -6,7 +6,7 @@ import kotlin.system.measureTimeMillis
 
 fun main() {
     measureTimeMillis {
-        // Download data from: https://data.cityofchicago.org/Public-Safety/Crimes-2021/dwme-t96c/data
+        // Download CSV data from: https://data.cityofchicago.org/Public-Safety/Crimes-2001-to-Present/ijzp-q8t2/data
         File("Crimes_-_2001_to_Present.csv").readLines()
             .drop(1)
             .map { it.split(",")[5] }
