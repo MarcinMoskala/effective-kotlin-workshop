f414b92fc20c89894291ae85ad8d0d184c489dec
marcinmoskala <marcinmoskala@gmail.com>
2021-11-17 12:00:20 +0100

    Better comment for nullability

--- src/main/kotlin/safe/Nullability.kt
+++ src/main/kotlin/safe/Nullability.kt
@@ -7,6 +7,7 @@ interface Mailer {
     fun sendMessage(email: String, message: String)
 }
 
+// Sends message if message and client's personal info email are not null
 fun sendMessageToClient(client: Client?, message: String?, mailer: Mailer) {
     TODO()
 }
