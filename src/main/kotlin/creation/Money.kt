3fce185f9df29c829b1ffb826514cb4cea068f44
marcinmoskala <marcinmoskala@gmail.com>
2021-11-23 18:32:01 +0100

    Operations to Money.kt

--- src/main/kotlin/creation/Money.kt
+++ src/main/kotlin/creation/Money.kt
@@ -8,6 +8,12 @@ data class Money(val amount: BigDecimal, val currency: Currency) {
 }
 
 fun main() {
+//    val m1 = Money(BigDecimal.TEN, Currency.EUR)
+//    val m2 = Money(BigDecimal.ONE, Currency.EUR)
+//    val m3 = m1 + m2
+//    val m4 = m1 - m2
+//    val m5 = m1 * BigDecimal.valueOf(2)
+
 //    val money1 = 10.pln()
 //    val money2 = 10.eur
 //    val money3 = 10.GBP
