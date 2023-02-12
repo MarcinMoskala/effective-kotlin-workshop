cf900b06a64b9a3c47362e45b55e2c71ee36fac4
marcinmoskala <marcinmoskala@gmail.com>
2023-02-12 18:32:50 +0100

    Add FunctionCaller task

--- src/test/kotlin/FunctionCallerTest.kt
+++ src/test/kotlin/FunctionCallerTest.kt
@@ -10,6 +10,7 @@ class FunctionCallerTest {
     fun callStringInt(str: String, int: Int) { value = "callStringInt $str $int" }
     data class User(val id: Int, val name: String?, var surname: String?)
     fun callUser(user: User) { value = user }
+    fun callWithDefault(c: Char, i: Int = 999, s: String = "XXX", l: Long) { value = "callWithDefault $c $i $s $l" }
 
     @AfterEach
     fun cleanUp() {
@@ -48,5 +49,14 @@ class FunctionCallerTest {
         caller.setConstant(user)
         caller.call(::callUser)
         assertEquals(user, value)
+    }
+
+    @Test
+    fun testIgnoredOptional() {
+        val caller = FunctionCaller()
+        caller.setConstant('Z')
+        caller.setConstant(123L)
+        caller.call(::callWithDefault)
+        assertEquals("callWithDefault Z 999 XXX 123", value)
     }
 }
